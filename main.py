1d7ca1f83c39f90d5cf5b0f8a9a14734f7db9edf
lonnie edwards <lonniece3@yahoo.com>
2023-11-09 05:09:06 -0700

    Create window and check for events.

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello World!")
+import pygame as pg
+
+pg.init()
+
+# Define properties
+x_axis = 800
+y_axis = 800
+bg_color = 'blue'
+run = True
+screen = pg.display.set_mode((x_axis, y_axis))
+
+
+# Game loop
+while run:
+    # Check for events every frame
+    for event in pg.event.get():
+
+        # End game when closed
+        if event.type == pg.QUIT:
+            run = False
