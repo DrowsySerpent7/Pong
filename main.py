b94c6406fb961d0cf5de48810350e7653af7f6b4
lonnie edwards <lonniece3@yahoo.com>
2023-11-13 20:04:36 -0700

    Player_1 sprite png added, new background color.

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import pygame as pg
 pg.init()
 
 # Define properties
-x_axis = 800
+x_axis = 1280
 y_axis = 800
-bg_color = 'blue'
 run = True
 screen = pg.display.set_mode((x_axis, y_axis))
+pg.display.set_caption("Pong")
+player = pg.image.load("player_pong_sprite.png" ).convert_alpha()
+player_rect = player.get_rect()
+player_rect.topleft = (50, 200)
+
+# player_pos = pg.Vector2(screen, screen.get_width() / 2, screen.get_height() / 2)
 
 
 # Game loop
@@ -18,3 +23,9 @@ while run:
         # End game when closed
         if event.type == pg.QUIT:
             run = False
+
+    screen.fill("gray")
+    screen.blit(player, (player_rect))
+    
+    pg.display.flip()
+    pg.display.update()
