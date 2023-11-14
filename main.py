d565c49b97fa08cec171901df0c337e576f5d689
lonnie edwards <lonniece3@yahoo.com>
2023-11-14 16:29:17 -0700

    Added ball and prep window collision.

--- main.py
+++ main.py
@@ -6,12 +6,21 @@ pg.init()
 x_axis = 1280
 y_axis = 800
 run = True
+clock = pg.time.Clock()
+ball_start_pos = (x_axis / 2, y_axis / 2)
+
+
 screen = pg.display.set_mode((x_axis, y_axis))
+screen.get_rect()
 pg.display.set_caption("Pong")
+
 player = pg.image.load("player_pong_sprite.png" ).convert_alpha()
 player_rect = player.get_rect()
 player_rect.topleft = (50, 200)
-clock = pg.time.Clock()
+
+game_ball = pg.image.load("ball_pong_sprite.png" ).convert_alpha()
+ball_circle = game_ball.get_rect()
+ball_circle.center = (ball_start_pos)
 
 
 def handle_keys():
@@ -34,6 +43,7 @@ while run:
 
 
     screen.fill("gray")
+    screen.blit(game_ball, (ball_circle))
     screen.blit(player, (player_rect))
     handle_keys()
     
