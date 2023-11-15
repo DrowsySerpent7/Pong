7f4ceb396a61cbd83b8352d2955d637d49b28e0c
lonnie edwards <lonniece3@yahoo.com>
2023-11-15 01:55:03 -0700

    Ball,Court,Lplayer,Rplayer,code clean up.

--- main.py
+++ main.py
@@ -6,30 +6,32 @@ pg.init()
 x_axis = 1280
 y_axis = 800
 run = True
+pg.display.set_caption("Pong")
 clock = pg.time.Clock()
-ball_start_pos = (x_axis / 2, y_axis / 2)
 
-
+pp_court = pg.image.load("pong_court.png")
 screen = pg.display.set_mode((x_axis, y_axis))
 screen.get_rect()
-pg.display.set_caption("Pong")
 
-player = pg.image.load("player_pong_sprite.png" ).convert_alpha()
-player_rect = player.get_rect()
-player_rect.topleft = (50, 200)
+playerl_pos = (80, 400)
+playerl = pg.image.load("player_pong_sprite.png" ).convert_alpha()
+playerl_rect = playerl.get_rect()
+playerl_rect.center = (playerl_pos)
 
 game_ball = pg.image.load("ball_pong_sprite.png" ).convert_alpha()
-ball_circle = game_ball.get_rect()
-ball_circle.center = (ball_start_pos)
+ball_start_pos = (x_axis / 2 + 2, y_axis / 2 - 2)
+game_ball_rect = game_ball.get_rect()
+game_ball_rect.center = (ball_start_pos)
 
 
+# Movement for playerL
 def handle_keys():
     """Handles key down logic for player movement."""
     keys = pg.key.get_pressed()
-    if keys[pg.K_UP]:
-        player_rect.move_ip(0, -6)
-    if keys[pg.K_DOWN]:
-        player_rect.move_ip(0, 6)
+    if keys[pg.K_w]:
+        playerl_rect.move_ip(0, -6)
+    if keys[pg.K_s]:
+        playerl_rect.move_ip(0, 6)
 
 
 # Game loop
@@ -42,11 +44,11 @@ while run:
             run = False
 
 
-    screen.fill("gray")
-    screen.blit(game_ball, (ball_circle))
-    screen.blit(player, (player_rect))
+    screen.blit(pp_court,(0, 0))
+    screen.blit(game_ball, game_ball_rect)
+    screen.blit(playerl, playerl_rect)
     handle_keys()
     
-    clock.tick(60)
+    clock.tick(65)
     pg.display.flip()
     pg.display.update()
